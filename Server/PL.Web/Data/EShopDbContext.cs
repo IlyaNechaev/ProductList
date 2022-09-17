a80dc2b5b30aef421d8ed1dd173a4582e01903e6
Ilya Nechaev <ilia.nechaeff@yandex.ru>
2022-09-17 17:19:35 +0300

    Init EF Core

---
+++ Server/PL.Web/Data/EShopDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PL.Web.Models;
+
+namespace PL.Web.Data;
+
+public class EShopDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+    public EShopDbContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        #region ORDER
+
+        // Первичный ключ
+        builder.Entity<Order>()
+            .HasKey(nameof(Order.ObjectID));
+
+        // Связь "многие-ко-многим"
+        builder.Entity<Order>()
+            .HasMany(nameof(Order.Products))
+            .WithMany(nameof(Product.Orders));
+
+        #endregion
+    }
+}
--- Server/PL.Web/Program.cs
+++ Server/PL.Web/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using PL.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Добавляем собственные сервисы
+#region CUSTOM_SERVICES
+
+var connectionString = builder.Configuration.GetConnectionString("Default");
+
+builder.Services.AddDbContext<EShopDbContext>(options =>
+{
+    options.UseSqlServer();
+})
+
+#endregion
 
 var app = builder.Build();
 
