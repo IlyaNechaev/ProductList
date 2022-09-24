8711b324c081b9b6a22098a95377174f610c331e
Ilya Nechaev <inechaev@croc.ru>
2022-09-24 18:29:59 +0300

    fix

--- Server/PL.Web/GlobalUsings.cs
+++ Server/PL.Web/GlobalUsings.cs
@@ -5,3 +5,6 @@ global using Microsoft.EntityFrameworkCore.SqlServer;
 global using PL.Web.Models;
 global using PL.Web.Data;
 global using PL.Web.Controllers;
+global using PL.Web.Services;
+global using PL.Web.Services.Interfaces;
+global using PL.Web.Services.Implements;
